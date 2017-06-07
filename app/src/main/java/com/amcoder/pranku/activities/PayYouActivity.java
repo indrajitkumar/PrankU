ba4368c4613956ccf04e548ad9ed8b994b35f3da
pabitra <pabitrakumar.sahoo@philips.com>
2017-06-07 11:43:24 +0530

    Added payU and active Android .

---
+++ app/src/main/java/com/amcoder/pranku/activities/PayYouActivity.java
@@ -0,0 +1,18 @@
+package com.amcoder.pranku.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by philips on 6/6/17.
+ */
+
+public class PayYouActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
